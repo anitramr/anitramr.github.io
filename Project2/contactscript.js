fae6862852636f24fc9b27969d7ff8eb048104a4
anitramr <72037936+anitramr@users.noreply.github.com>
2020-12-07 20:37:38 -0800

    Update contactscript.js

--- Project2/contactscript.js
+++ Project2/contactscript.js
@@ -1,5 +1,48 @@
 'use strict'
 
+
+
+function toggleIcon() {
+    $('.icon').on('click', function(){
+        $('.icon').toggleClass('active');
+        $('.menu-mobile').slideToggle(300);
+    })
+}
+
+function closeMobileMenu(){
+    $('.menu-mobile').on('click', 'a', function(){
+        $('.icon').trigger('click');
+    })
+}
+
+
+
+function desktopMenu() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0 && $(window).width() > 768) {
+            $('header.desktop').fadeIn(500);
+        } else {
+            $('header.desktop').fadeIn(500);
+        }
+    })
+}
+
+
+
+
+function init(){
+    desktopMenu();
+    closeMobileMenu()
+    toggleIcon()
+}
+
+$(init())  
+
+
+
+
+
+
 var section = $('li');
 
 function toggleAccordion() {
